fb1e7337e3ace9b0f68c3032c9e41f4b3bd80174
Dheeraj-kumar001 <dheerajsonkar272@gmail.com>
2025-10-09 08:09:33 +0530

    update route_tables.tf

---
+++ Terraform-aws-eks/route_tables.tf
@@ -0,0 +1,43 @@
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.ecommerce_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = { Name = "public-rt" }
+}
+
+resource "aws_route_table_association" "public1_assoc" {
+  subnet_id      = aws_subnet.ecommerce_subnet1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2_assoc" {
+  subnet_id      = aws_subnet.ecommerce_subnet2.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.ecommerce_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = { Name = "private-rt" }
+}
+
+resource "aws_route_table_association" "private1_assoc" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private2_assoc" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private.id
+}
